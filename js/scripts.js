f33ef25cafe3d57a20f4aa231f9905531deb63e3
Alex Lee <doomcat9167@gmail.com>
2017-03-05 13:14:39 -0800

    modifying typedjs to look like imessage font. Still in testing branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 //jQuery
 $(function() {
     //delay load four items
-    $("#vic").delay(500).show('scale', 1000);
-    $("#header").hide();
-    $("#header").delay(3000).show('fade', 1000);
-    $("#yes").delay(2000).show('fade', 1000).typed({
-        strings: ["Word up."],
-        typeSpeed: 100
-    });
-
-    $("#buttons").delay(3000).show('fade', 1000);
+    // $("#vic").delay(500).show('scale', 1000);
+    // $("#header").hide();
+    // $("#header").delay(3000).show('fade', 1000);
+    // $("#typed").delay(2000).show('fade', 1000).typed({
+    //     strings: ["Yo Lee, you full-stack?"],
+    //     typeSpeed: 70
+    // });
+    //
+    // $("#buttons").delay(3000).show('fade', 1000);
+    $("#typed").typed({
+        stringsElement: $('#typed-strings'),
+        typeSpeed: 50,
+        backDelay: 500,
+        contentType: 'html', // or text
+        // defaults to false for infinite loop
+    }); //typed.js
 }); //jQuery
