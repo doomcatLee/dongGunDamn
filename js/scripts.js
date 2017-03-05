e26113cb466b2414fc2f8f0269382f2d59a0bb79
Alex Lee <doomcat9167@gmail.com>
2017-03-05 11:35:00 -0800

    fix row and column spaces. Better organized for easy css

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //jQuery
 $(function() {
     //delay load four items
-    $("#vic").delay(500).show('scale', 1000);
-    $("#header").hide();
-    $("#header").delay(2500).show('fade', 1000);
-    $("#yes").delay(1500).show('fade', 1000);
-    $("#navi").delay(2500).show('fade', 1000);
+    // $("#vic").delay(500).show('scale', 1000);
+    // $("#header").hide();
+    // $("#header").delay(2500).show('fade', 1000);
+    // $("#yes").delay(1500).show('fade', 1000);
+    // $("#buttons").delay(2500).show('fade', 1000);
 }); //jQuery
