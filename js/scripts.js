0cb40d29155a88eea01c664c9c407802ec55b99c
Alex Lee <doomcat9167@gmail.com>
2017-03-05 13:26:42 -0800

    fix spacings and minor stuff. LOOKS FUCKIN SICK

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
 //jQuery
 $(function() {
     //delay load four items
-    // $("#vic").delay(500).show('scale', 1000);
-    // $("#header").hide();
-    // $("#header").delay(3000).show('fade', 1000);
-    // $("#typed").delay(2000).show('fade', 1000).typed({
-    //     strings: ["Yo Lee, you full-stack?"],
-    //     typeSpeed: 70
-    // });
-    //
-    // $("#buttons").delay(3000).show('fade', 1000);
+    $("#vic").delay(500).show('scale', 1000);
+    $(".type-wrap").delay(1500).show('fade');
+    $("#header").delay(3500).show('fade', 1000);
+    $("#yes").delay(4800).show('fade', 1000);
+
+    $("#buttons").delay(3500).show('fade', 1000);
     $("#typed").typed({
         stringsElement: $('#typed-strings'),
         typeSpeed: 50,
         backDelay: 500,
+        startDelay: 2300,
         contentType: 'html', // or text
         // defaults to false for infinite loop
     }); //typed.js
