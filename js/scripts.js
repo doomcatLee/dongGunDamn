9f436619873fb0b0dc3e7c5c324ac7d9dc40f155
Alex Lee <doomcat9167@gmail.com>
2017-03-05 12:19:46 -0800

    add typed.js to modify front page. SET POINT HERE, WILL BRANCH OFF FOR TESTING

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 //jQuery
 $(function() {
     //delay load four items
-    // $("#vic").delay(500).show('scale', 1000);
-    // $("#header").hide();
-    // $("#header").delay(2500).show('fade', 1000);
-    // $("#yes").delay(1500).show('fade', 1000);
-    // $("#buttons").delay(2500).show('fade', 1000);
+    $("#vic").delay(500).show('scale', 1000);
+    $("#header").hide();
+    $("#header").delay(3000).show('fade', 1000);
+    $("#yes").delay(2000).show('fade', 1000).typed({
+        strings: ["Word up."],
+        typeSpeed: 100
+    });
+
+    $("#buttons").delay(3000).show('fade', 1000);
 }); //jQuery
