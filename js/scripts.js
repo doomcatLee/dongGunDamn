bcf0d03c4d9f568af54347ac09bbde6eb53f4bc9
Alex Lee <doomcat9167@gmail.com>
2017-03-05 19:19:09 -0800

    add bio shit to about page

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,17 @@
-// //Play music when starts after a delay
-// var sound = document.createElement('audio')
-// sound.id = 'audio'
-// sound.controls = 'controls'
-// sound.src = 'http://a.tumblr.com/tumblr_leltkjNwWL1qf32t9o1.mp3'
-// sound.type = 'audio/mp3'
-// document.body.appendChild(sound)
-//
-// function playAudio() {
-//     document.getElementById('audio').play();
-// }
-
-setTimeout("playAudio()", 3000);
-
 //jQuery
 $(function() {
     //delay load four items
-    $("#vic").delay(500).show('scale', 1000);
-    $(".type-wrap").delay(1500).show('fade');
-    $("#header").delay(5500).show('fade', 1000);
-    $("#yes").delay(4800).show('fade', 1000);
+    $("#vic").delay(500).show('bounce', 1000);
+    $(".type-wrap").delay(1600).show('fade');
+    $("#header").delay(6700).show('fade', 1500);
+    $("#yes").delay(5500).show('fade', 1500);
 
-    $("#buttons").delay(5500).show('fade', 1000);
+    $("#buttons").delay(6700).show('fade', 1500);
     $("#typed").typed({
         stringsElement: $('#typed-strings'),
         typeSpeed: 50,
         backDelay: 500,
         startDelay: 2300,
-        contentType: 'html', // or text
-        // defaults to false for infinite loop
+        contentType: 'html'
     }); //typed.js
 }); //jQuery
