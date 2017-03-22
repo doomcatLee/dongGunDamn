2b78588a665485b188c8bd5227d1ae6391cd24be
Alex Lee <doomcat9167@gmail.com>
2017-03-22 00:04:49 -0700

    add correct delay time for each rows

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@
 $(function() {
     //delay load four items
 
+    $(".row-victor").delay(1000).show("fade", 2500);
+    $(".row-niggaLee").delay(5000).show("fade", 2500);
+    $(".row-button").delay(8000).show("fade", 3000);
+    $(".row-nav").delay(8000).show("fade", 3000);
 
     $("#typedLee").typed({
         stringsElement: $('#typed-Lee'),
         typeSpeed: 50,
         backDelay: 500,
-        startDelay: 5500,
+        startDelay: 6500,
         contentType: 'html'
     }); //typed.js
     $("#typedVic").typed({
